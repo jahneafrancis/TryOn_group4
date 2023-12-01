f1b808ee5f77306a26e6fffe5d9d2cede3ede334
vlxcoding <vlxcoding@gmail.com>
2023-12-01 14:28:18 -0500

    Integrated AI Chatbot into Landing Page and modified frontend

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,63 +1,162 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName("update-cart");
 
 for (i = 0; i < updateBtns.length; i++) {
-	updateBtns[i].addEventListener('click', function(){
-		var productId = this.dataset.product
-		var action = this.dataset.action
-		console.log('productId:', productId, 'Action:', action)
-		console.log('USER:', user)
+  updateBtns[i].addEventListener("click", function () {
+    var productId = this.dataset.product;
+    var action = this.dataset.action;
+    console.log("productId:", productId, "Action:", action);
+    console.log("USER:", user);
 
-		if (user == 'AnonymousUser'){
-			addCookieItem(productId, action)
-		}else{
-			updateUserOrder(productId, action)
-		}
-	})
+    if (user == "AnonymousUser") {
+      addCookieItem(productId, action);
+    } else {
+      updateUserOrder(productId, action);
+    }
+  });
 }
 
-function updateUserOrder(productId, action){
-	console.log('User is authenticated, sending data...')
+function updateUserOrder(productId, action) {
+  console.log("User is authenticated, sending data...");
 
-		var url = '/update_item/'
+  var url = "/update_item/";
 
-		fetch(url, {
-			method:'POST',
-			headers:{
-				'Content-Type':'application/json',
-				'X-CSRFToken':csrftoken,
-			}, 
-			body:JSON.stringify({'productId':productId, 'action':action})
-		})
-		.then((response) => {
-		   return response.json();
-		})
-		.then((data) => {
-		    location.reload()
-		});
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": csrftoken,
+    },
+    body: JSON.stringify({ productId: productId, action: action }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      location.reload();
+    });
 }
 
-function addCookieItem(productId, action){
-	console.log('User is not authenticated')
+function addCookieItem(productId, action) {
+  console.log("User is not authenticated");
 
-	if (action == 'add'){
-		if (cart[productId] == undefined){
-		cart[productId] = {'quantity':1}
+  if (action == "add") {
+    if (cart[productId] == undefined) {
+      cart[productId] = { quantity: 1 };
+    } else {
+      cart[productId]["quantity"] += 1;
+    }
+  }
 
-		}else{
-			cart[productId]['quantity'] += 1
-		}
-	}
+  if (action == "remove") {
+    cart[productId]["quantity"] -= 1;
 
-	if (action == 'remove'){
-		cart[productId]['quantity'] -= 1
+    if (cart[productId]["quantity"] <= 0) {
+      console.log("Item should be deleted");
+      delete cart[productId];
+    }
+  }
+  console.log("CART:", cart);
+  document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
 
-		if (cart[productId]['quantity'] <= 0){
-			console.log('Item should be deleted')
-			delete cart[productId];
-		}
-	}
-	console.log('CART:', cart)
-	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
-	
-	location.reload()
+  location.reload();
 }
+
+// Try-On AI Implementation
+
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+
+let userMessage = null; // Variable to store user's message
+const API_KEY = "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"; // Paste your API key here
+const inputInitHeight = chatInput.scrollHeight;
+
+const createChatLi = (message, className) => {
+  // Create a chat <li> element with passed message and className
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", `${className}`);
+  let chatContent =
+    className === "outgoing"
+      ? `<p></p>`
+      : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+  chatLi.innerHTML = chatContent;
+  chatLi.querySelector("p").textContent = message;
+  return chatLi; // return chat <li> element
+};
+
+const generateResponse = (chatElement) => {
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+  const messageElement = chatElement.querySelector("p");
+
+  // Define the properties and message for the API request
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: userMessage }],
+    }),
+  };
+
+  // Send POST request to API, get response and set the reponse as paragraph text
+  fetch(API_URL, requestOptions)
+    .then((res) => res.json())
+    .then((data) => {
+      messageElement.textContent = data.choices[0].message.content.trim();
+    })
+    .catch(() => {
+      messageElement.classList.add("error");
+      messageElement.textContent =
+        "Oops! Something went wrong. Please try again.";
+    })
+    .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+};
+
+const handleChat = () => {
+  userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+  if (!userMessage) return;
+
+  // Clear the input textarea and set its height to default
+  chatInput.value = "";
+  chatInput.style.height = `${inputInitHeight}px`;
+
+  // Append the user's message to the chatbox
+  chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+
+  setTimeout(() => {
+    // Display "Thinking..." message while waiting for the response
+    const incomingChatLi = createChatLi("Thinking...", "incoming");
+    chatbox.appendChild(incomingChatLi);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    generateResponse(incomingChatLi);
+  }, 600);
+};
+
+chatInput.addEventListener("input", () => {
+  // Adjust the height of the input textarea based on its content
+  chatInput.style.height = `${inputInitHeight}px`;
+  chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+  // If Enter key is pressed without Shift key and the window
+  // width is greater than 800px, handle the chat
+  if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+    e.preventDefault();
+    handleChat();
+  }
+});
+
+sendChatBtn.addEventListener("click", handleChat);
+closeBtn.addEventListener("click", () =>
+  document.body.classList.remove("show-chatbot")
+);
+chatbotToggler.addEventListener("click", () =>
+  document.body.classList.toggle("show-chatbot")
+);
